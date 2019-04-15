504aa30ec53b12f216ed4b34123d7c1478e5f051
big.baby-@hotmail.com <30289807+biggywk@users.noreply.github.com>
2019-04-16 01:14:08 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.38ee9603fd3119ce9ba398702e5eb6cf.js"
+  "/https://biggywk.github.io/Bangladesh/precache-manifest.05ec50a2f02218d1fbf775f25880e1e0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/https://biggywk.github.io/Bangladesh/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
