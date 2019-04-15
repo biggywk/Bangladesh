e5b7a2af2cf11b63e2a2ca698e20e5db1011bdc8
big.baby-@hotmail.com <30289807+biggywk@users.noreply.github.com>
2019-04-15 14:47:49 +0700

    Updates

---
+++ precache-manifest.e75d27dcfecd3a12b04908b949a147cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stat_group3.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d8c20703a7692b7579ec",
+    "url": "/stat_group3.github.io/static/js/runtime~main.63af14f9.js"
+  },
+  {
+    "revision": "0edf21691f054653ee84",
+    "url": "/stat_group3.github.io/static/js/main.d9bc3b51.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/stat_group3.github.io/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0edf21691f054653ee84",
+    "url": "/stat_group3.github.io/static/css/main.59ed27f1.chunk.css"
+  },
+  {
+    "revision": "c113dff7046dd8eed270ae014335b290",
+    "url": "/stat_group3.github.io/index.html"
+  }
+];
