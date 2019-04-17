059d161ff7dfb1bca68617201bfc9467a217dd05
big.baby-@hotmail.com <30289807+biggywk@users.noreply.github.com>
2019-04-17 16:36:44 +0700

    Updates

---
+++ precache-manifest.835c23cfe06689ba20cba7207c932589.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "8836139e917c43039176",
+    "url": "./static/js/main.d399132e.chunk.js"
+  },
+  {
+    "revision": "7c1754c00221493b7dc0",
+    "url": "./static/js/2.f20a288c.chunk.js"
+  },
+  {
+    "revision": "8836139e917c43039176",
+    "url": "./static/css/main.4a61caa5.chunk.css"
+  },
+  {
+    "revision": "66d4b537f624deba1000af8cf80c69fd",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.42f5a1c0dc6a17525a0fef987664ff2f.js"
+  "./precache-manifest.835c23cfe06689ba20cba7207c932589.js"
 );
 
 workbox.clientsClaim();
